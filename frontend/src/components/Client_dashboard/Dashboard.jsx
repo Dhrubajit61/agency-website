06eeb2f957276ab042e863bc410b49b6365e7906
Dhrubajit61 <Dhrubajit61@gmail.com>
2025-04-09 12:09:33 +0530

    after adding logout animation

--- frontend/src/components/Client_dashboard/Dashboard.jsx
+++ frontend/src/components/Client_dashboard/Dashboard.jsx
@@ -1,10 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Preloader2 from "../Home/Preloader2";
+import "../../assets/css/login.css";
+import "../../assets/css/Modal.css";
+import { useContext } from "react";
+import { Openloginmodalcontext } from "../Home/Contextapi";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const apiUrl = "http://127.0.0.1:8000";
+  const [loading, setLoading] = useState(false);
+  const { isLoginModalOpen, setIsLoginModalOpen } = useContext(
+    Openloginmodalcontext
+  );
+  const [successfullogout, setSuccessfullogout] = useState(false);
 
   useEffect(() => {
     const checkToken = async () => {
@@ -36,11 +46,66 @@ const Dashboard = () => {
     checkToken();
   }, [navigate]);
 
+  const handlelogoutclick = () => {
+    localStorage.removeItem("access_token");
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setSuccessfullogout(true);
+    }, 500);
+
+    setTimeout(() => {
+      navigate("/login");
+    }, 2000);
+  };
+  const handleOverlayClick = (e) => {
+    // Check if click is outside the modal area
+    if (e.target.classList.contains("modal-overlay")) {
+      handleCloseModal();
+    }
+  };
+  const handleCloseModal = () => {
+    setIsLoginModalOpen(false);
+  };
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to your dashboard! You are authenticated.</p>
-    </div>
+    <>
+      <div style={{ position: "relative" }}>
+        {loading && (
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              zIndex: 1000,
+            }}
+          >
+            <div style={{ color: "#fff", fontSize: "24px" }}>
+              <Preloader2></Preloader2>
+            </div>
+          </div>
+        )}
+      </div>
+      {successfullogout == false ? (
+        <div>
+          <h1>Dashboard</h1>
+          <p>Welcome to your dashboard! You are authenticated.</p>
+          <button onClick={handlelogoutclick}>Logout here</button>
+        </div>
+      ) : (
+        <div className="modal-overlay">
+          <div className="modal login-modal">
+            <h1>Logged out successfully</h1>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
