177e371a725e2c561940f5c3840ae00f8998601d
Dhrubajit61 <Dhrubajit61@gmail.com>
2025-04-15 13:02:19 +0530

    added colorful cards in the DashboardHome components

--- frontend/src/components/Client_dashboard/DashboardHome.jsx
+++ frontend/src/components/Client_dashboard/DashboardHome.jsx
@@ -7,6 +7,40 @@ const DashboardHome = () => {
   const clickme = () => {
     console.log(user.user.name);
   };
-  return <>{user && <h3>Welcome! {user.user.name}</h3>}</>;
+  return (
+    <>
+      {user && (
+        <div>
+          <h3>Welcome! {user.user.name}</h3>
+          <div className="Dashboard-cards">
+            <div className="Dashboard-card-elements">
+              <div className="Dashboard-card-elements-div">
+                <p>25</p>
+                <p>Total Requirements Submitted </p>
+              </div>
+            </div>
+            <div className="Dashboard-card-elements">
+              <div className="Dashboard-card-elements-div">
+                <p>25</p>
+                <p>Active Projects </p>
+              </div>
+            </div>
+            <div className="Dashboard-card-elements">
+              <div className="Dashboard-card-elements-div">
+                <p>25</p>
+                <p>Completed Projects </p>
+              </div>
+            </div>
+            <div className="Dashboard-card-elements">
+              <div className="Dashboard-card-elements-div">
+                <p>25</p>
+                <p>Unread Messages </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 export default DashboardHome;
